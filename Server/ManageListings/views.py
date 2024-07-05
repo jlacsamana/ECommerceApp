7f0e65e0766c07a86b6890557faf2f38ae0c42f9
Jacob Lacsamana <jacoblacsamana0@gmail.com>
2024-07-05 16:26:00 -0700

    switch backend to django

---
+++ Server/ManageListings/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+# Create your views here.
+
+def test(request):
+    return JsonResponse({'message': 'Test Successful: Test Successful: Listings Working'})
