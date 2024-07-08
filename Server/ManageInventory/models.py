20e69924983d1c8efd82bdecba1805837cd930ac
Jacob Lacsamana <jacoblacsamana0@gmail.com>
2024-07-08 15:42:01 -0700

    .

--- Server/ManageInventory/models.py
+++ Server/ManageInventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class InventoryItem(models.Model):
+    """standard inventory item"""
+
+    upc = models.CharField(max_length=26)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    is_second_hand = models.BooleanField(default=False)
+
+    # optional fields
+    manufacturer = models.CharField(max_length=100, blank=True, null=True)
+    product_series = models.CharField(max_length=100, blank=True, null=True)
+
+    quantity = models.IntegerField(default=1)
+
+    class Meta:
+        unique_together = (("upc", "is_second_hand"),)
--- Server/ManageListings/models.py
+++ Server/ManageListings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from ManageInventory.models import InventoryItem
+
 
 # Create your models here.
+class Listing(models.Model):
+    """a (cross)post for an item"""
+
+    item = models.ForeignKey(InventoryItem, on_delete=models.CASCADE)
+    price = models.FloatField()
+
+    # is posting listed on these platforms?
+    onWooCommerce = models.BooleanField(default=False)
+    onEbay = models.BooleanField(default=False)
+    onAmazon = models.BooleanField(default=False)
+    onFBMarketplace = models.BooleanField(default=False)
